1d7a226712dcf83d452ffc55ccb6687aad0bc771
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2024-04-10 21:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let submitBtn = document.querySelector("#submit");
+let checkBox = document.querySelector("#checkbox");
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let loginBtn = document.querySelector("#existing");
+
+submitBtn.addEventListener('click',(event)=>{
+	event.preventDefault();
+// if(username.value === '' || password.value === ''){}
+  if(checkbox.checked){
+  let users =[];
+  let user ={
+   UserName : username.value,
+   Password : password.value}
+   users.push(user);
+   localStorage.setItem('users', JSON.stringify(users));
+  }
+  else if(JSON.parse(localStorage.getItem('users'))){
+  	let savedUser =	JSON.parse(localStorage.getItem('users'));
+    savedUser.forEach((user)=>{
+      if(user.UserName === username.value ){      
+	    loginBtn.removeAttribute('hidden')
+      loginBtn.addEventListener('click',(e)=>{
+  		  e.preventDefault();
+  		  alert(`Logged in as ${user.UserName}`)})
+      }
+    });
+  }
+  else{
+		alert(`Logged in as ${username.value}`);
+    }
+});
